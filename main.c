ab8f5abe7660b0f449dc7c4e346524ce20adec6e
Bahie <mahmoudelbahaie402@gmail.com>
2024-09-15 20:54:38 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define hamada 10
+
+int main (){
+     int x = hamada ;
+     int y = 45 ;
+     printf("X value = %i\n" , x );
+     printf("y value = %i\n" , y );
+     return 0 ;
+}
